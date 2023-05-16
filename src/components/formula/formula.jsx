3028de3a2665ebd539f2f8c8d67114c64ac9068b
rqphy <raphael.ferreira@hetic.net>
2023-05-16 13:34:51 +0200

    :art: set formula to wireframe

--- src/components/formula/formula.jsx
+++ src/components/formula/formula.jsx
@@ -1,7 +1,18 @@
 import { useGLTF } from "@react-three/drei"
+import { useEffect } from "react"
 
 export default function Formula() {
 	const model = useGLTF("./f1/scene.gltf")
+
+	useEffect(() => {
+		// Set wireframe material on all meshes in the model
+		model.scene.traverse((child) => {
+			if (child.isMesh) {
+				child.material.wireframe = true
+			}
+		})
+	}, [])
+
 	return (
 		<primitive
 			object={model.scene}
