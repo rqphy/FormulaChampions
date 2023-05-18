c4cb17e0b59e8f24426a6a1de471170c24f7b32e
rqphy <raphael.ferreira@hetic.net>
2023-05-18 22:37:27 +0200

    :sparkles: add page title

---
+++ src/components/title/title.jsx
@@ -0,0 +1,7 @@
+export default function Title() {
+	return (
+		<h1 className="title">
+			<span>F1</span>World Champions
+		</h1>
+	)
+}
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,8 +1,10 @@
 import Experience from "../../components/experience/experience"
+import Title from "../../components/title/title"
 
 export default function Home() {
 	return (
 		<div className="Home">
+			<Title />
 			<Experience />
 		</div>
 	)
