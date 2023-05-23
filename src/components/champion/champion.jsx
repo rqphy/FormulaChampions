3494114e7b9618e6557e4e5c11704d162974f0cb
rqphy <raphael.ferreira@hetic.net>
2023-05-23 17:07:27 +0200

    :art: update champion 3d text font

--- src/components/champion/champion.jsx
+++ src/components/champion/champion.jsx
@@ -1,15 +1,16 @@
 import { Text } from "@react-three/drei"
 
-const scale = 0.08
+const scale = 0.1
 
 export default function Champion({ name, index }) {
 	return (
 		<Text
-			position={[-1, index * -3 * scale + 1, 3]}
+			position={[-1, index * -2 * scale + 1, 3]}
 			color="black"
 			scale={scale}
 			anchorX="left"
 			anchorY="top"
+			font="font/HUMANE/Web-TT/Humane-Bold.ttf"
 		>
 			{name}
 		</Text>
