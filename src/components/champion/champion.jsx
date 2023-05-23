8841d2a791bba3690c31d80aed252826e0c261fe
rqphy <raphael.ferreira@hetic.net>
2023-05-23 16:51:11 +0200

    :art: update champion style

--- src/components/champion/champion.jsx
+++ src/components/champion/champion.jsx
@@ -1,11 +1,13 @@
 import { Text } from "@react-three/drei"
 
+const scale = 0.08
+
 export default function Champion({ name, index }) {
 	return (
 		<Text
-			position={[-2, index * -0.5 + 2, 0]}
+			position={[-1, index * -3 * scale + 1, 3]}
 			color="black"
-			scale={0.2}
+			scale={scale}
 			anchorX="left"
 			anchorY="top"
 			key={index}
